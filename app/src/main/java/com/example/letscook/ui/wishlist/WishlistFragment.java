d41251171b072874ec26937b7f19db4c85d1bf21
duc140032 <dangcongduc090@gmail.com>
2021-10-24 19:47:06 +0700

    update con

--- app/src/main/java/com/example/letscook/ui/wishlist/WishlistFragment.java
+++ app/src/main/java/com/example/letscook/ui/wishlist/WishlistFragment.java
@@ -8,29 +8,48 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.letscook.R;
 import com.example.letscook.databinding.FragmentWishlistBinding;
+
+import java.util.ArrayList;
 
 public class WishlistFragment extends Fragment {
 
     private WishlistViewModel wishlistViewModel;
     private FragmentWishlistBinding binding;
 
+    RecyclerView recyclerView;
+    ArrayList<CongThuc> listSanPham;
+    CongThucAdapter congThucAdapter;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         wishlistViewModel =
                 new ViewModelProvider(this).get(WishlistViewModel.class);
 
+
         binding = FragmentWishlistBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        final TextView textView = binding.textWishlist;
-//        wishlistViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+        recyclerView= root.findViewById(R.id.recyclerview);
+        listSanPham=new ArrayList<>();
+        listSanPham.add(new CongThuc(R.drawable.hambeger, "ga cay"));
+        listSanPham.add(new CongThuc(R.drawable.hambeger, "ga cay1"));
+        listSanPham.add(new CongThuc(R.drawable.hambeger, "ga cay2"));
+        listSanPham.add(new CongThuc(R.drawable.hambeger, "ga cay3"));
+        listSanPham.add(new CongThuc(R.drawable.hambeger, "ga cay"));
+        listSanPham.add(new CongThuc(R.drawable.hambeger, "ga cay1"));
+        listSanPham.add(new CongThuc(R.drawable.hambeger, "ga cay2"));
+        listSanPham.add(new CongThuc(R.drawable.hambeger, "ga cay3"));
+        congThucAdapter=new CongThucAdapter(getContext(),listSanPham);
+        recyclerView.setAdapter(congThucAdapter);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(null, 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
         return root;
     }
 
