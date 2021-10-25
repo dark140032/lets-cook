0e77603ec70caa290b821eedd207fe093b8e6138
KhanhToan2310 <khanhtoantruong196@gmail.com>
2021-10-25 23:41:47 +0700

    coding ui and config db

---
+++ app/src/main/java/com/example/letscook/MyDB.java
@@ -0,0 +1,28 @@
+package com.example.letscook;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDB extends SQLiteOpenHelper {
+
+    private Context context;
+    private  static final String DATABASE_NAME = "LetCook.db";
+    private  static final int DATABASE_VERSION = 1;
+
+
+    public MyDB(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
